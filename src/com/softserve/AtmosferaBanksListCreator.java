f08f59c99ce6b58767741852de45bbeec1d2cf42
Ivanna <ivaifua@users.noreply.github.com>
2014-10-23 02:02:27 +0300

    Update AtmosferaBanksListCreator.java

--- src/com/softserve/AtmosferaBanksListCreator.java
+++ src/com/softserve/AtmosferaBanksListCreator.java
@@ -1,78 +1,9 @@
 package com.softserve;
 
-/*
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-
-public class AtmosferaBanksListCreator implements IBanksListCreator {
-    private String networkUrl;
-
-    AtmosferaBanksListCreator(String networkUrl){
-        this.networkUrl = networkUrl;
-    }
-
-    public List<BankEntry> createBanksList() {
-        // TODO: write method
-        List<BankEntry> atmosferaBanks = new ArrayList<BankEntry>();
-        try {
-            Document doc = Jsoup.connect(networkUrl).get();
-            //Elements table = doc.select("tr.tab_top");
-            Elements rows = doc.select("tr.tab_top");
-            Element title = rows.get(0);
-            Element table_row = title.nextElementSibling();
-            while ( table_row != null){
-                Elements name = table_row.select("td.tab_line_mini");
-                System.out.println(name.html());
-                table_row = table_row.nextElementSibling();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return atmosferaBanks;
-    }
-
-    /*
-    public List<BankEntry> createBanksList(){
-        // TODO: write method
-        List<BankEntry> atmosferaBanks = new ArrayList<BankEntry>();
-
-
-
-        try {
-            Document doc = Jsoup.connect(networkUrl).get();
-            Elements table = doc.select("tbody");
-            Elements rows = table.select("tr");
-            Elements names = rows.select("td.tab_line_mini");
-            Elements links = rows.select("td.a");
-
-            for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
-                Element row = rows.get(i);
-                Element name = names.get(i);
-                Element link = links.get(i);
-
-                atmosferaBanks.add(new BankEntry(name.text(), link.attr("href")));
-            }
-
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
-
-        return null; // List!
-    }
-}
-*/
+import java.util.ArrayList;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -83,6 +14,7 @@ import org.jsoup.select.Elements;
 /**
  * Created by Ivanna Terletska on 22.10.2014.
  */
+
 public class AtmosferaBanksListCreator implements IBanksListCreator {
     private String networkUrl;
 
@@ -92,34 +24,27 @@ public class AtmosferaBanksListCreator implements IBanksListCreator {
     }
 
     public List<BankEntry> createBanksList() {
-        // TODO: write method
+        
         List<BankEntry> atmosferaBanks = new ArrayList<BankEntry>();
+        
         try {
-
             Document doc = Jsoup.connect(networkUrl).get();
-
-            Elements rows = doc.select("tr.tab_top");
-            Element title = rows.get(0);
-            Element table_row = title.nextElementSibling();
-
-            while ( table_row != null){
-
-                Elements name = table_row.select("td.tab_line_mini");
-                //System.out.println(name.html());
-                table_row = table_row.nextElementSibling();
-
-                Elements cols = table_row.select("td");
-                Element link = cols.get(2).select("a").get(0);
-                System.out.println(name.text()+";"+link.attr("href"));
-                atmosferaBanks.add(new BankEntry(name.text(), link.attr("href")));
+            Elements names = doc.select("td.tab_line_mini");  // Array of names of banks
+            
+            for (Element name : names) { 
+            	Element  tmp = name.nextElementSibling();
+            	String   bankName = name.text();
+            	String   bankUrl = tmp.child(0).attr("href");
+              //  System.out.println(bankName);
+              //  System.out.println(url);
+                atmosferaBanks.add(new BankEntry(bankName,bankUrl));    //Add elements to list
+                tmp = tmp.nextElementSibling();
             }
-
-
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        return null; // List!
+        return atmosferaBanks; // List!
     }
 }
